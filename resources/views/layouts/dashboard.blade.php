04ba7f3a39a5fd99285cc18d4696dabe91ebce83
shuvam <shuvamdas.219@gmail.com>
2020-07-31 12:10:04 +0530

    1st commit

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!-- Meta Data -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="xcoder">
+    <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel dolorem dolor, facilis fuga tenetur">
+    <meta name="author" content="Shuvam Das">
+    <!-- Meta Data End -->
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }} - A platform for practice</title>
+
+    <!-- Scripts -->
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+
+
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                        @include('layouts.partials._nav')
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                        <li class="nav-item">
+                            <a class="nav-link color-green" href="{{ route('login') }}"><i class="fas fa-sign-in-alt"></i></a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
---
+++ resources/views/layouts/partials/_nav.blade.php
@@ -0,0 +1,2 @@
+<li class="nav-item active"><a class="nav-link" href="{{ route('about') }}">About</a></li>
+<li class="nav-item active"><a class="nav-link" href="{{ route('contact') }}">Contact Us</a></li>
